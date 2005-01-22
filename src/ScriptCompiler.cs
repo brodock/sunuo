8403b47b60a4bdc15ade1402e93e000ee13bec9f
Max Kellermann <max@duempel.org>
2005-01-22 11:07:50 +0000

    keywords set
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@10 45309df1-3fee-0310-8168-e5b643bb5cc7

--- src/ScriptCompiler.cs
+++ src/ScriptCompiler.cs
@@ -6,9 +6,9 @@
  *                          (C) 2005 Max Kellermann <max@duempel.org>
  *   email                : max@duempel.org
  *
- *   $Id: ScriptCompiler.cs,v 1.4 2005/01/22 04:25:04 krrios Exp $
- *   $Author: krrios $
- *   $Date: 2005/01/22 04:25:04 $
+ *   $Id$
+ *   $Author$
+ *   $Date$
  *
  *
  ***************************************************************************/
