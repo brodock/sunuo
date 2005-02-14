9181230fadb69182c9a5db3a477323a2ff0db547
Max Kellermann <max@duempel.org>
2005-02-14 21:08:22 +0000

    show mono warning message
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@87 45309df1-3fee-0310-8168-e5b643bb5cc7

--- src/Main.cs
+++ src/Main.cs
@@ -349,6 +349,9 @@ namespace Server
 			Console.WriteLine("  on {0}, runtime {1}",
 							  Environment.OSVersion, Environment.Version);
 
+			if ((int)Environment.OSVersion.Platform == 128)
+				Console.WriteLine("Please make sure you have Mono 1.1.3 or newer! (mono -V)");
+
 			while ( !ScriptCompiler.Compile( debug ) )
 			{
 				Console.WriteLine( "Scripts: One or more scripts failed to compile or no script files were found." );
