2bf8dc22f1abcd71e4d0e1b7d66bc9f0d1b70e2d
Max Kellermann <max@duempel.org>
2005-05-08 15:47:51 +0000

    compile only ./src/
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@154 45309df1-3fee-0310-8168-e5b643bb5cc7

--- compile.bat
+++ compile.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 
-%windir%\Microsoft.NET\Framework\v1.1.4322\csc.exe /nologo /out:SunUO.exe /unsafe /recurse:*.cs
+cd src
+
+%windir%\Microsoft.NET\Framework\v1.1.4322\csc.exe /nologo /out:../SunUO.exe /unsafe /recurse:*.cs
 
 PAUSE
 
