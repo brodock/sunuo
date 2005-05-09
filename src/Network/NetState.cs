27ed50394bc977461c6b08d1145d84dc6828d194
Max Kellermann <max@duempel.org>
2005-05-09 08:05:33 +0000

    added author
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@165 45309df1-3fee-0310-8168-e5b643bb5cc7

--- src/Network/NetState.cs
+++ src/Network/NetState.cs
@@ -3,11 +3,12 @@
  *                            -------------------
  *   begin                : May 1, 2002
  *   copyright            : (C) The RunUO Software Team
- *   email                : info@runuo.com
+ *                          (C) 2005 Max Kellermann <max@duempel.org>
+ *   email                : max@duempel.org
  *
- *   $Id: NetState.cs,v 1.8 2005/01/22 04:25:04 krrios Exp $
- *   $Author: krrios $
- *   $Date: 2005/01/22 04:25:04 $
+ *   $Id$
+ *   $Author$
+ *   $Date$
  *
  *
  ***************************************************************************/
