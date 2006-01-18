cf1df72e1f4b3b9ec037d4e49ae86b700f40084b
Max Kellermann <max@duempel.org>
2006-01-18 10:34:40 +0000

    link with log4net.dll
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@326 45309df1-3fee-0310-8168-e5b643bb5cc7

--- compile.bat
+++ compile.bat
@@ -3,7 +3,7 @@
 set FRAMEWORK=%windir%\Microsoft.NET\Framework\v1.1.4322
 
 cd src
-%FRAMEWORK%\csc.exe /nologo /out:SunUO.exe /unsafe /recurse:*.cs
+%FRAMEWORK%\csc.exe /nologo /out:SunUO.exe /lib:..\build\lib /r:log4net.dll /unsafe /recurse:*.cs
 
 cd ..\util
 %FRAMEWORK%\csc.exe /nologo /out:UOGQuery.exe /unsafe UOGQuery.cs
