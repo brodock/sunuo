bb2fe485c6125e76b705aec18fb4f221a2e90b22
Max Kellermann <max@duempel.org>
2006-02-03 17:11:24 +0000

    updated .NET 2.0 path
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@479 45309df1-3fee-0310-8168-e5b643bb5cc7

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 set FRAMEWORK=%windir%\Microsoft.NET\Framework\v1.1.4322
-REM set FRAMEWORK=%windir%\Microsoft.NET\Framework64\v2.0.50215
+REM set FRAMEWORK=%windir%\Microsoft.NET\Framework64\v2.0.50727
 
 cd src
 %FRAMEWORK%\csc.exe /nologo /out:SunUO.exe /lib:..\build\lib /r:log4net.dll /recurse:*.cs
