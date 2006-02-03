5891193d8867dc7f7c548a15be7395159fb3ff2f
Max Kellermann <max@duempel.org>
2006-02-03 17:14:28 +0000

    compile to build\
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@480 45309df1-3fee-0310-8168-e5b643bb5cc7

--- compile.bat
+++ compile.bat
@@ -3,11 +3,13 @@
 set FRAMEWORK=%windir%\Microsoft.NET\Framework\v1.1.4322
 REM set FRAMEWORK=%windir%\Microsoft.NET\Framework64\v2.0.50727
 
+mkdir build
+
 cd src
-%FRAMEWORK%\csc.exe /nologo /out:SunUO.exe /lib:..\build\lib /r:log4net.dll /recurse:*.cs
+%FRAMEWORK%\csc.exe /nologo /out:..\build\SunUO.exe /lib:..\build\lib /r:log4net.dll /recurse:*.cs
 
 cd ..\util
-%FRAMEWORK%\csc.exe /nologo /out:UOGQuery.exe UOGQuery.cs
+%FRAMEWORK%\csc.exe /nologo /out:..\build\UOGQuery.exe UOGQuery.cs
 
 cd ..
 
