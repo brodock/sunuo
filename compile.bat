1733e04ae91309f5e1edd5f10916bd70c1ca8535
Max Kellermann <max@duempel.org>
2006-02-03 18:37:14 +0000

    w32 compiles with full debug info
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@484 45309df1-3fee-0310-8168-e5b643bb5cc7

--- compile.bat
+++ compile.bat
@@ -6,10 +6,10 @@ REM set FRAMEWORK=%windir%\Microsoft.NET\Framework64\v2.0.50727
 mkdir build
 
 cd src
-%FRAMEWORK%\csc.exe /nologo /out:..\build\SunUO.exe /lib:..\build\lib /r:log4net.dll /recurse:*.cs
+%FRAMEWORK%\csc.exe /nologo /debug:full /out:..\build\SunUO.exe /lib:..\build\lib /r:log4net.dll /recurse:*.cs
 
 cd ..\util
-%FRAMEWORK%\csc.exe /nologo /out:..\build\UOGQuery.exe UOGQuery.cs
+%FRAMEWORK%\csc.exe /nologo /debug:full /out:..\build\UOGQuery.exe UOGQuery.cs
 
 cd ..
 
