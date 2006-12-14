29ff2db0255c7804fd5d1a4e24ed12394d71afde
Max Kellermann <max@duempel.org>
2006-12-14 23:11:09 +0000

    run SunUO in a loop
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@700 45309df1-3fee-0310-8168-e5b643bb5cc7

---
+++ sunuo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#  SunUO
+#  $Id$
+#
+#  (c) 2006 Max Kellermann <max@duempel.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+# a few checks
+
+if ! [ -f SunUO.exe ]; then
+    echo "ERROR: SunUO.exe does not exist" >&2
+    exit 2
+fi
+
+if ! [ -f SunUO.exe.config ]; then
+    echo "Warning: SunUO.exe.config does not exist" >&2
+fi
+
+# prepare environment
+
+export LANG=C
+export LC_ALL=C
+
+unset DISPLAY XAUTHORITY
+
+# our main loop
+while :; do
+    # start SunUO.exe
+    ${MONO:-mono} ${MONO_OPTS:---server --debug -O=all,-shared} SunUO.exe "$@"
+    echo "SunUO.exe exited with status $?" >&2
+
+    # exit status 99 means "really exit, do not restart"
+    [ $? == 99 ] && break
+
+    # sleep for a reasonable duration and restart
+    sleep 10
+    echo "Re-starting SunUO.exe"
+done
+
+echo "Leaving $0" >&2
