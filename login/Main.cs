d8098a7af62e28f286af9da5c7573c39c6de946c
Max Kellermann <max@duempel.org>
2006-12-25 13:18:13 +0000

    new header
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@809 45309df1-3fee-0310-8168-e5b643bb5cc7

--- login/Main.cs
+++ login/Main.cs
@@ -1,26 +1,24 @@
-/***************************************************************************
- *                                  Main.cs
- *                            -------------------
- *   begin                : May 1, 2002
- *   copyright            : (C) The RunUO Software Team
- *                          (C) 2005 Max Kellermann <max@duempel.org>
- *   email                : max@duempel.org
+/*
+ * SunUO
+ * $Id$
  *
- *   $Id$
- *   $Author: make $
- *   $Date$
+ * (c) 2005-2006 Max Kellermann <max@duempel.org>
+ * based on code (C) The RunUO Software Team
  *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
  *
- ***************************************************************************/
-
-/***************************************************************************
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- ***************************************************************************/
+ */
 
 using System;
 using System.IO;
