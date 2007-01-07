bce194d0f66eede17a95b055dcde11a173abb1c0
Max Kellermann <max@duempel.org>
2007-01-07 18:47:23 +0000

    dont allow save intervals < 1
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@822 45309df1-3fee-0310-8168-e5b643bb5cc7

--- src/Config.cs
+++ src/Config.cs
@@ -659,7 +659,7 @@ namespace Server.Config {
 
 					case "save-interval":
 						double si = Convert.ToDouble( el.GetAttribute("value") );
-						if(si <= 0.1) {
+						if(si <= 1.0) {
 							log.WarnFormat("Invalid value of save-interval, setting it to default");
 						}
 						else {
