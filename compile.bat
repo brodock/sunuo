60429c9aefd1b64e0280e3bf1c2149509b76a461
Max Kellermann <max@duempel.org>
2007-05-22 11:14:02 +0000

    dont build UOGQuery.exe on windows
    git-svn-id: svn://svn.berlios.de/sunuo/trunk@854 45309df1-3fee-0310-8168-e5b643bb5cc7

--- compile.bat
+++ compile.bat
@@ -8,9 +8,6 @@ mkdir build
 cd src
 %FRAMEWORK%\csc.exe /nologo /debug:full /out:..\build\SunUO.exe /lib:..\lib /r:log4net.dll /recurse:*.cs
 
-cd ..\util
-%FRAMEWORK%\csc.exe /nologo /debug:full /out:..\build\UOGQuery.exe UOGQuery.cs
-
 cd ..
 
 PAUSE
